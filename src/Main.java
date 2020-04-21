156f918d215bd5bc339ad1a83a6036db60d13cfc
zengjin <1129304859.qq.com>
2020-04-21 09:12:28 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+import	java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : zengjin
+ * @date : 2020-03-26 10:55
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Map<String,Integer> map = new HashMap<> ();
+        map.put("K1",1);
+        map.put("K2",2);
+        map.put("K3",3);
+        map.put("K4",4);
+        map.put("K5",5);
+    }
+}
